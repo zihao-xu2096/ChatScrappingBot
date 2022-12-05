b72d43970c79677dac0a74e0a1d8be40f494a38a
Harry Xu <harryxcz@gmail.com>
2022-12-05 02:47:27 -0500

    Implement Typed.js api in hero section of Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,15 +2,40 @@ import React from 'react';
 import videoBg from '../assets/twitchchat.mp4'
 import Typed from 'typed.js'
 export default function Home() {
-  return (
+
+    const el = React.useRef(null);
+    const typed = React.useRef(null);
+
+    React.useEffect(() => {
+        const options = {
+            strings: [
+                'Youtube',
+                'Twitch',
+                'Facebook Gaming'
+            ],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true,
+            showCursor: false,
+            smartBackspace: true
+        };
+    
+
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+        typed.current.destroy();
+    }
+    }, [])
+    return (
     <body>
         <div className='mainvideo'>
             <video className='twitchvid' src={videoBg} autoPlay loop muted playsInline></video>
         </div>
-        <h2 className='hero'> Get chat informatics from vods, clips on  <span class='auto-type'></span></h2>
-        <div className='About-Section'>
-            <h3> How does it work?</h3>
+        <h2 className='hero'> Get live chat informatics from vods and clips from:  <span ref={el}></span> </h2>
+        <div className='About'>
+            <h3 className='About-Header'> How does it work?</h3>
         </div>
     </body>
-  )
+    )
 }

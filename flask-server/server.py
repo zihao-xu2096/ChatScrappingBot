6897f41993414e6410b7647a04977926e769955b
zihao-xu2096 <harryxcz@gmail.com>
2023-01-04 16:22:04 -0800

    Initialize an HTTP POST request to flask-server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,21 @@ app = Flask(__name__)
 @app.route("/members", methods=['GET'])
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
+
+# Twitchlink API route
+
+
+app.config['JSON_AS_ASCII'] = False
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route("/twitchlink", methods=['POST', 'GET'])
+def link():
+    if request.method == 'POST':
+        form_data = request.get_json(force=True)
+        return "Success"
+    else:
+        return "Called for the GET request"
 
 
 if __name__ == "__main__":
